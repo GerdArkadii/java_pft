3ecdc8d98181f5ed2d5464754fde1a54bdb90e49
GerdArkadii <gerdarkadiy@gmail.com>
2023-01-23 09:34:33 +0300

    Первый урок

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Da.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Da.java
@@ -2,6 +2,23 @@ package ru.stqa.pft.sandbox;
 
 public class Da {
     public static void main(String[] args) {
-        System.out.println("aaaaaa");
+        hello("world");
+        hello("user");
+        double l = 5;
+        Square s = new Square(5);
+        System.out.println("Квадрат = " + s.area());
+        Rectangle r = new Rectangle(4,5);
+        r.a = 4;
+        r.b = 5;
+        System.out.println("Площадь = " + r.a +" "+  r.b +" "+  r.area());
+
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+
+
+
 }
